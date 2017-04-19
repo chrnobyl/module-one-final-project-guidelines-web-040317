5787d55ef9ae47dc39a342349aac590bb5610a52
kylewstewart <kyle.w.stewart@mac.com>
2017-04-19 13:57:47 -0400

    whats upgit add .git add .

--- bin/run.rb
+++ bin/run.rb
@@ -1,4 +1,15 @@
-require_relative 'config/environment'
+require_relative '../config/environment'
 
-puts "Enter a player name to retrieve stats: "
-input = gets.chomp
+puts "Enter a team name: "
+name = gets.chomp
+puts "Enter a year: "
+year = gets.chomp
+
+def get_season(name, year)
+  binding.pry
+ team = Team.find_by(name: name)
+ team_season = team.seasons.find_by(year: year)
+ puts team_season
+end
+
+get_season(name, year)
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 require 'bundler'
 Bundler.require
 
-ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/database.sqlite')
-require_all 'app'
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: '../db/database.sqlite')
+# require_all 'app'
+
+require_relative File.join(File.dirname(__FILE__), "..", "app/models", "team.rb")
+require_relative File.join(File.dirname(__FILE__), "..", "app/models", "season.rb")
