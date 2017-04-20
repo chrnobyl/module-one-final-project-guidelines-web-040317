e570bcbb0a0448733c09a50ef200d3ceade0e547
github username <github email address>
2017-04-20 15:03:42 -0400

    Formatting with colors etc

--- bin/run.rb
+++ bin/run.rb
@@ -26,18 +26,57 @@ features = [
   ["Team with the most runs scored in a given year", :runs]
 ]
 
-puts "Welcome to every baseball statistic ever. Please choose one of the following stats:"
-features.each_with_index do |feature, index|
-  puts "#{index + 1} - #{feature[0]}"
+
+puts "        _                    _           _ _
+       | |                  | |         | | |
+       | |__   __ _ ___  ___| |__   __ _| | |
+       | '_ \\ / _` / __|/ _ \\ '_ \\ / _` | | |
+       | |_) | (_| \\__ \\  __/ |_) | (_| | | |
+       |_.__/ \\__,_|___/\\___|_.__/ \\__,_|_|_|
+                                             ".bold.red
+
+while true
+
+  puts "Welcome to every baseball statistic ever. Please choose one of the following stats. Type 'exit' at any time to quit the program.".bold.red
+  features.each_with_index do |feature, index|
+    puts "#{index + 1} - #{feature[0]}"
+  end
+
+  puts "Enter the number of the stat you want:"
+  feature = gets.chomp
+    if feature == "exit"
+      break
+    end
+  feature = feature.to_i
+
+  puts "Please enter a year from 1871 - 2016 (YYYY):"
+  year = gets.chomp
+  if year == "exit"
+    break
+  end
+  year = year.to_i
+
+  if year.to_s.length != 4
+    puts "Please enter a year in YYYY format:"
+    year = gets.chomp
+    if year == "exit"
+      break
+    end
+    year = year.to_i
+
+  elsif year.to_i < 1871 || year.to_i > 2016
+    puts "No records exist for that year. Please enter a valid year:"
+    year = gets.chomp
+    if year == "exit"
+      break
+    end
+    year = year.to_i
+  else
+    most = team_most_stat_year(features[feature - 1][1], year)
+    stat_str = features[feature - 1][1].to_s.split('_').join(" ")
+    puts "The " + "#{most[0]}".green + " had " + "#{most[1]}".green + " #{stat_str}.".green
+    break
+  end
 end
 
-puts "Enter the number of the stat you want:"
-feature = gets.chomp.to_i
-
-puts "Please enter a year (YYYY):"
-year = gets.chomp
-
-most = team_most_stat_year(features[feature - 1][1], year)
-
-stat_str = features[feature - 1][1].to_s.split('_').join(" ")
-puts "The #{most[0]} had #{most[1]} #{stat_str}"
+puts "Bye"
