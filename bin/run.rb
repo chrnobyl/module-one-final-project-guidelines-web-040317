f2104ac93c93dacea85aa1bf375f5c87262ce624
kylewstewart <kyle.w.stewart@mac.com>
2017-04-20 14:43:46 -0400

    further abstraction and added avgs

--- bin/run.rb
+++ bin/run.rb
@@ -1,106 +1,71 @@
 require_relative '../config/environment'
 ActiveRecord::Base.logger.level = 1
 
-def stat_by_team_by_year(statistic, year)
-  return nil if !check_stat(statistic)
-  team_stat = {}
-  Team.joins(:seasons).where(seasons: {year: year}).each do |team|
-    team_stat[team.name] = Season.where(team_id: team.id, year: year).first[statistic]
-  end
-  team_stat
+
+def features
+  {
+  max_by_year: [
+  ["The team with the most home runs in a given year", "home_runs"],
+  ["Team with the most wins in a given year", "wins"],
+  ["Team with the most strike outs in a given year", "strike_outs"],
+  ["Team with the most losses in a given year", "losses"],
+  ["Team with the most runs scored in a given year", "runs"]
+  ],
+  avg_by_year: [
+    ["The average home runs per team in a given year", "home_runs"],
+    ["Team average wins per team in a given year", "wins"],
+    ["Team average strike outs per team in a given year", "strike_outs"],
+    ["Team average losses per team in a given year", "losses"],
+    ["Team average runs scored per team in a given year", "runs"]
+  ]
+  }
 end
 
-def check_stat(statistic)
-  Season.column_names().collect {|column| column.to_sym}.include?(statistic)
+
+def max_team_stat_year(statistic, year)
+  max_stat = Season.where(year: year).maximum(statistic)
+  max_team = Team.joins(:seasons).where(seasons: {year: year, statistic => max_stat})
+  {statistic => max_stat, team: max_team.first.name}
 end
 
-# def team_most_strikeouts_by_year(year)
-#   team_strikeouts_by_year(:strike_outs, year).max_by{|team, so| so}
-# end
-#
-# def team_most_wins_by_year(year)
-#   team_wins_by_year(:wins, year).max_by{|team, wins| wins}
-# end
-#
-# def team_most_hr_by_year(year)
-#   stat_by_team_by_year(:home_runs, year).max_by{|team, hr| hr}
-# end
-
-def team_most_stat_year(statistic, year)
-  stat_by_team_by_year(statistic, year).max_by{|team, statistic| statistic}
+def avg_stat_year(statistic, year)
+  Season.where(year: year).average(statistic)
 end
 
-features = [
-  ["The team that hit the most home runs in a given year", :home_runs],
-  ["Team with the most wins in a given year", :wins, ],
-  ["Team with the most strike outs in a given year", :strike_outs],
-  ["Team with the most losses in a given year", :losses],
-  ["Team with the most runs scored in a given year", :runs]
-]
+def max_by_year
+  puts "Available max stats by year:"
+  features[:max_by_year].each_with_index {|feature, index| puts "#{index + 1} - #{feature[0]}"}
 
-  # "Total Home Runs hit is a given year and average by team"
+  puts "Please enter the stat you want (by number):"
+  num = gets.chomp.to_i
+  puts "Please enter a year (YYYY):"
+  year = gets.chomp
 
-
-puts "Welcome to every baseball statistic ever. Please choose one of the following stats:"
-features.each_with_index do |feature, index|
-  puts "#{index + 1} - #{feature[0]}"
+  statistic = features[:max_by_year][num - 1][1]
+  max = max_team_stat_year(statistic, year)
+  puts "The #{max[:team]} had #{max[statistic]} #{statistic.split('_').join(' ')}"
 end
 
-puts "Enter the number of the stat you want:"
+def avg_by_year
+  puts "Available avg stats by year:"
+  features[:avg_by_year].each_with_index {|feature, index| puts "#{index + 1} - #{feature[0]}"}
+
+  puts "Please enter the stat you want (by number):"
+  num = gets.chomp.to_i
+  puts "Please enter a year (YYYY):"
+  year = gets.chomp
+
+  statistic = features[:avg_by_year][num - 1][1]
+  avg = avg_stat_year(statistic, year)
+  puts "The average team in #{year} had #{avg} #{statistic}"
+end
+
+
+
+puts "Welcome to every baseball statistic ever. Please choose a category of stats:"
+features.keys.each_with_index {|key, index| puts "#{index + 1} - #{key.to_s.split('_').join(' ')}"}
+
+puts "Enter the number of the category you want:"
 feature = gets.chomp.to_i
 
-puts "Please enter a year (YYYY):"
-year = gets.chomp
-
-most = team_most_stat_year(features[feature - 1][1], year)
-
-stat_str = features[feature - 1][1].to_s.split('_').join(" ")
-puts "The #{most[0]} had #{most[1]} #{stat_str}"
-
-
-# case feature
-# when 1
-#     puts ask.red
-#     year = gets.chomp
-#     most_hr = team_most_hr_by_year(year)
-#     puts "In #{year}, the #{most_hr[0]} hit an astounding #{most_hr[1]} home runs."
-#     # binding.pry
-#   when 2
-#     puts ask.red
-#     year = gets.chomp
-#     most_wins = team_most_wins_by_year(year)
-#     puts "In #{year}, the #{most_wins[0]} won #{most_wins[1]} games - the most wins of that year."
-#   when 3
-#     puts ask.red
-#     year = gets.chomp
-#     most_strikeouts = team_most_strikeouts_by_year(year)
-#     puts "In #{year}, the #{most_strikeouts[0]} struck out #{most_strikeouts[1]} batters."
-#   else
-#     puts "Goodbye."
-# end
-
-
-
-
-# def get_season(name, year)
-#  team = Team.find_by(name: name)
-#  team_season = team.seasons.find_by(year: year)
-# end
-#
-# def league_array
-#   Season.select(:league).distinct.collect do |season|
-#     season.league
-#   end
-# end
-
-
-# team_most_hr_by_year(1986)
-
-
-
-
-# season = get_season(name, year)
-# puts "Wins: #{season.wins}"
-
-
-# Team.joins(:seasons).where(seasons: {year: (2000..2016)}).uniq
+send(features.keys[feature -1])
