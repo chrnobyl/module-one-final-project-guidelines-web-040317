838a81d602d4fc4a778faeab2f50d7070119554a
github username <github email address>
2017-04-21 14:36:57 -0400

    made even more formatting changes

--- bin/run.rb
+++ bin/run.rb
@@ -84,54 +84,122 @@ def teams_by_year(year)
 end
 
 def simple_linear_regression
-  puts "Enter the starting year(YYYY):"
-  start_year = gets.chomp.to_i
-  puts "Enter the ending year(YYYY):"
-  end_year = gets.chomp.to_i
+  puts "Enter the starting year (YYYY):".green
+  start_year = gets.chomp
+  if start_year == "exit"
+    return "exit"
+  end
+  until YEAR_RANGE.include?(start_year.to_i) == true
+    puts "There are no stats for that year. Please enter a year from the 1871 - 2016:".green
+    start_year = gets.chomp
+  break if start_year == "exit"
+  end
+  if start_year == "exit"
+    return "exit"
+  end
+  start_year.to_i
 
-  puts "Available statistics to test:"
+  puts "Enter the ending year (YYYY):".green
+  end_year = gets.chomp
+  if end_year == "exit"
+    return "exit"
+  end
+  until YEAR_RANGE.include?(end_year.to_i) == true
+    puts "There are no stats for that year. Please enter a year from the 1871 - 2016:".green
+    end_year = gets.chomp
+  break if end_year == "exit"
+  end
+  if end_year == "exit"
+    return "exit"
+  end
+  end_year.to_i
+
+  puts "Available statistics to test:".green
+
+  linstats_indices = []
   features[:simple_linear_regression].each_with_index do |feature, index|
     puts "#{index + 1} - #{feature[0]}"
+    linstats_indices << (index + 1).to_s
   end
 
-  puts "Choose the independent statistic (by number):"
-  y_num = gets.chomp.to_i
+  puts "Choose the independent statistic (by number):".green
+  y_num = gets.chomp
+  if y_num == "exit"
+    return "exit"
+  end
+  until linstat_indices.include?(y_num) == true
+    puts "That stat is not listed. Please enter a number from the list of statistics:".green
+    y_num = gets.chomp
+  break if y_num == "exit"
+  end
+  if y_num == "exit"
+    return "exit"
+  end
+  y_num = y_num.to_i
+
   y_stat = features[:simple_linear_regression][y_num - 1][1]
   y_stat_name = features[:simple_linear_regression][y_num - 1][0]
 
-  puts "Choose the dependent statistic (by number):"
+  puts "Choose the dependent statistic (by number):".green
   x_num = gets.chomp.to_i
   x_stat = features[:simple_linear_regression][x_num - 1][1]
   x_stat_name = features[:simple_linear_regression][x_num - 1][0]
 
   regression = linear_regression(start_year, end_year, y_stat, x_stat)
 
-  puts "A set of #{regression[0]} data pairs were analyzed for the period of #{start_year} to #{end_year}."
-  puts "During this period, #{y_stat_name} and #{x_stat_name} have correlation of #{regression[1].round(2)}"
+  puts "
+A set of " + "#{regression[0]}".bold.blue + " data pairs were analyzed for the period of " + "#{start_year}".bold.blue + " to " + "#{end_year}".bold.blue + ".
+  "
+  puts "During this period, " + "#{y_stat_name}".bold.blue + " and " + "#{x_stat_name}".bold.blue + " have a correlation of " + "#{regression[1].round(2)}".bold.blue + ".
+  "
 end
 
 def compare_a_team_to_avg
-  puts "Please enter a year (YYYY):".green
+  puts "Please enter a year from 1871 - 2016 (YYYY):".green
   year = gets.chomp
   if year == "exit"
     return "exit"
   end
+  until YEAR_RANGE.include?(year.to_i) == true
+    puts "There are no stats for that year. Please enter a year from the 1871 - 2016:".green
+    year = gets.chomp
+  break if year == "exit"
+  end
+  if year == "exit"
+    return "exit"
+  end
+
+  comp_indices = []
   teams = teams_by_year(year)
-  teams.each_with_index {|team, index| puts "#{index + 1} - #{team.name}"}
+  teams.each_with_index do |team, index|
+    puts "#{index + 1} - #{team.name}"
+    comp_indices << (index + 1).to_s
+  end
 
   puts "Please select a team (by number):".green
   num = gets.chomp
   if num == "exit"
     return "exit"
   end
+  until comp_indices.include?(num) == true
+    puts "That team is not listed. Please enter a number from the list of teams:".green
+    num = gets.chomp
+  break if num == "exit"
+  end
+  if num == "exit"
+    return "exit"
+  end
   num = num.to_i
   team = teams[num - 1]
 
-  puts "In #{year} the #{team.name} had:".green
+  puts "
+In #{year} the #{team.name} had:
+  ".bold.blue
   features[:compare_a_team_to_avg].each do |feature|
     avg_stat = avg_stat_year(feature[1], year)
     team_stat = Season.where(team_id: team.id, year: year).first[feature[1]]
-    puts "#{team_stat}".green + " #{feature[0]}".green + " vs. an average of " + "#{avg_stat}".green + "."
+    puts "#{team_stat}".bold.blue + " #{feature[0]}".bold.blue + " vs. an average of " + "#{avg_stat}".bold.blue + ".
+    "
   end
 end
 
@@ -156,7 +224,7 @@ def max_by_year
   end
   num = num.to_i
 
-  puts "Please enter a year between 1871 - 2016 (YYYY):".green
+  puts "Please enter a year from 1871 - 2016 (YYYY):".green
   year = gets.chomp
   if year == "exit"
     return "exit"
@@ -172,29 +240,53 @@ def max_by_year
 
   statistic = features[:max_by_year][num - 1][1]
   max = max_team_stat_year(statistic, year)
-  puts "The #{max[:team]} had #{max[statistic]} #{statistic.split('_').join(' ')}"
+  puts "
+The " +"#{max[:team]}".bold.blue + " had " + "#{max[statistic]} #{statistic.split('_').join(' ')}".bold.blue + ".
+  "
 end
 
 def avg_by_year
- puts "Available avg stats by year:".green
- features[:avg_by_year].each_with_index {|feature, index| puts "#{index + 1} - #{feature[0]}"}
+  aby_indices = []
+  puts "Available avg stats by year:".green
+  features[:avg_by_year].each_with_index do |feature, index|
+    puts "#{index + 1} - #{feature[0]}"
+    aby_indices << (index + 1).to_s
+  end
 
  puts "Please enter the stat you want (by number):".green
  num = gets.chomp
  if num == "exit"
    return "exit"
  end
+ until aby_indices.include?(num) == true
+   puts "That stat does not exist. Please enter a number from the category list:".green
+   num = gets.chomp
+ break if num == "exit"
+ end
+ if num == "exit"
+   return "exit"
+ end
  num = num.to_i
 
- puts "Please enter a year (YYYY):".green
+ puts "Please enter a year from 1871 - 2016 (YYYY):".green
  year = gets.chomp
+ if year == "exit"
+   return "exit"
+ end
+ until YEAR_RANGE.include?(year.to_i) == true
+   puts "There are no stats for that year. Please enter a year from the 1871 - 2016:".green
+   year = gets.chomp
+ break if year == "exit"
+ end
  if year == "exit"
    return "exit"
  end
 
  statistic = features[:avg_by_year][num - 1][1]
  avg = avg_stat_year(statistic, year)
- puts "The average team in " + "#{year}".bold.green + " had " + "#{avg} #{statistic}".bold.green + "."
+ puts "
+The average team in " + "#{year}".bold.blue + " had " + "#{avg} #{statistic.split("_").join(" ")}".bold.blue + ".
+ "
 end
 
 
@@ -237,4 +329,6 @@ while var != "exit"
 
 end
 
-puts "Ok bye.".bold.red
+puts "
+Ok bye.
+".bold.red
